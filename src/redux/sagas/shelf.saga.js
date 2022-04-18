066e69140fbd2dc3adad729bc52fab62b5a295b3
Andy Swanson <swan1625@icloud.com>
2022-04-18 15:04:05 -0500

    cleaned up some code

--- src/redux/sagas/shelf.saga.js
+++ src/redux/sagas/shelf.saga.js
@@ -6,8 +6,10 @@ function* fetchShelfItems() {
 
     try {
         const shelf = yield axios.get('/api/shelf')
+        
         // console.log('get all:', shelf.data);
         yield put({ type: 'SET_SHELF_ITEMS', payload: shelf.data });
+
         // console.log('after reducer is called, still in saga');
 
     } catch {
